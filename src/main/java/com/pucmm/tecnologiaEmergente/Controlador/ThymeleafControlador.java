c850fe633c90a6e4e80a83bd5d9b78f72825dce1
PR0C3S <20180767@ce.pucmm.edu.do>
2022-06-22 15:01:53 -0400

    Query Consumo diario integracion en Spring

--- src/main/java/com/pucmm/tecnologiaEmergente/Controlador/ThymeleafControlador.java
+++ src/main/java/com/pucmm/tecnologiaEmergente/Controlador/ThymeleafControlador.java
@@ -1,5 +1,8 @@
 package com.pucmm.tecnologiaEmergente.Controlador;
 
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.pucmm.tecnologiaEmergente.Models.Complementos.Almacen;
 import com.pucmm.tecnologiaEmergente.Models.Complementos.DetalleInventario;
 import com.pucmm.tecnologiaEmergente.Models.Componente;
@@ -8,7 +11,10 @@ import com.pucmm.tecnologiaEmergente.Repositorios.ComponenteRepositorio;
 import com.pucmm.tecnologiaEmergente.Repositorios.MovimientoInventarioRepositorio;
 import com.pucmm.tecnologiaEmergente.Repositorios.OrdenesRepositorio;
 import com.pucmm.tecnologiaEmergente.Repositorios.SuplidorRepositorio;
+import org.bson.Document;
+import org.bson.json.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +22,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.websocket.server.PathParam;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.*;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @Controller
 @RequestMapping(path="/crud")
@@ -42,6 +51,7 @@ public class ThymeleafControlador {
         if(ordenesCarrito==null){
             ordenesCarrito = new HashMap<Componente, Integer>();
         }
+
         model.addAttribute("lista",ordenesCarrito);
         if(!alerta.equals("")){
             model.addAttribute("alerta",alerta);
@@ -62,8 +72,17 @@ public class ThymeleafControlador {
             return "redirect:/crud/generarOrden";
         }
 
+        //calcular dias
+        LocalDate today = LocalDate.now(ZoneId.systemDefault());
+        LocalDate anterior = today.minusYears(3);
+        AggregationResults<Document> output = movimientoInventarioRepositorio.findByTipoMovimientoAndFecha(7,anterior,today);
+        Document dc = output.getUniqueMappedResult();
+        int cantidad = (int) dc.get("totalConsumidos");
+        int dias= (int) DAYS.between(anterior, today);
+        int consumoDiario =cantidad/dias;
 
         //VERIFICACION QUE NO SE HAYA GENERADO UN PEDIDO PARA ESA FECHA
+
 
         //FILTRO DE CALIDAD PRECIO CON AGGREGATES
 
@@ -79,6 +98,51 @@ public class ThymeleafControlador {
 
         return "";
     }
+
+//    entrada: (codigoProducto, cantidad_stock_que_quiero, fecha_para_la_que_se_quiere) List<Peticion> //Peticion seria un nuevo objeto disenado para recibir este tipo de peticiones // fecha seria igual para todos los productos
+//
+//new List<OrdenCompra>;//lista vacia
+//
+//    for peticion in List<Peticion>{
+//
+//        detalle = new DetallesOrden();
+//        consumo_diario = find_consumo_diario(codigoComponente); >> mongo
+//                consumo_total = dias_hasta_fecha_objetivo * consumo //se podria conseguir consumo total directo desde el query en mongo.
+//
+//        if ((stockEsperado - consumo_total) > peticion.cantidad_stock_que_quiero){
+//            //ya va haber suficiente stock, me salto esta peticion
+//
+//        }else{// si no va a haber suficiente stock, busco el mejor suplidor
+//            mejor_suplidor = find_mejor_suplidor(peticion.codigoComponente, peticion.fecha) >> mongo //find_mejor_suplidor no retorna un Suplidor en si, si no un objeto Json con toda la info que necesito.
+//
+//            llenar parametros de detalle;//con mejor_suplidor lleno los parametros de detalle(codigoSuplidor, precio, descuento...), almacen al que se pide es un numero random del 1 al 3.
+//
+//            //falta manejo de caso cuando no encuentra un suplidor.
+//            if find_mejor_suplidor == null{
+//                //saltar a siguiente peticion.
+//            }
+//
+//            guardar(detalle);
+//            //actualizar Componente.ordenados[] con el recien creado DetalleOrden
+//            componente = find_Componente(peticion.codigoComponente);
+//            componente.ordenados.append(new Obj(codigoAlmacen, cantidadComprada, mejor_suplidor.fechaEntrega)); //almacen al que se pide es un numero random del 1 al 3.
+//
+//            if (mejor_suplidor.codigoSuplidor == orden.codigoSuplidor){//si ya hay una orden creada para mejor_suplidor, agrego ese numeroOrden al recien creado DetalleOrden
+//
+//            }else{//si no, creo una nueva Orden y agrego ese numeroOrden al recien creado DetalleOrden
+//                nueva_orden = new Orden();
+//                //lleno los parametros de nueva_orden;
+//                List<OrdenCompra>.append(nueva_orden);
+//            }
+//        }
+//    }
+//
+//    for orden in List<OrdenCompra>{
+//            guardar orden;
+//        }
+//
+//    return List<OrdenCompra>; // o void
+//        salida: void
 
 //    @PostMapping("/generarMovimiento")
 //    public String generarMovimiento(
--- src/main/java/com/pucmm/tecnologiaEmergente/Repositorios/MovimientoInventarioRepositorio.java
+++ src/main/java/com/pucmm/tecnologiaEmergente/Repositorios/MovimientoInventarioRepositorio.java
@@ -1,14 +1,38 @@
 package com.pucmm.tecnologiaEmergente.Repositorios;
 
-import com.pucmm.tecnologiaEmergente.Models.Complementos.DetalleInventario;
 import com.pucmm.tecnologiaEmergente.Models.MovimientoInventario;
-import com.pucmm.tecnologiaEmergente.Models.Suplidor;
+import org.bson.Document;
+import org.bson.json.JsonObject;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 public interface MovimientoInventarioRepositorio extends MongoRepository<MovimientoInventario, String> {
 
+
+    @Aggregation(pipeline = {
+            "{$unwind: {path: \"$detalle\"}}",
+            "{$match: {\n" +
+                    "      tipoMovimiento: \"SALIDA\",\n" +
+                    "      \"detalle.codigoComponente\": ?0,\n" +
+                    "      \"fechaMovimiento\": { $gte: ?1, $lte: ?2 }\n" +
+                    "    }}",
+            " {$group: {\n" +
+                    "     _id: {codigoComponente:\"$detalle.codigoComponente\"},\n" +
+                    "     numMovimiento: {$sum: 1},\n" +
+                    "     totalConsumidos: {$sum: \"$detalle.cantidadMovimiento\"}\n" +
+                    "    }}",
+            "{$project: {\n" +
+                    "       _id:0,\n" +
+                    "       codigoComponente: \"$_id.codigoComponente\",\n" +
+                    "       numMovimiento: \"$numMovimiento\",\n" +
+                    "       totalConsumidos: \"$totalConsumidos\",\n" +
+                    "    }}"
+    })
+    AggregationResults<Document> findByTipoMovimientoAndFecha(int codigoComponente, LocalDate fechaInf, LocalDate fechaSup);
 
 }
