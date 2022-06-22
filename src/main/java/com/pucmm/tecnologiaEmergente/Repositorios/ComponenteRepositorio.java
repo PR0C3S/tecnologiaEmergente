54703b4bb40b432b822aaf8a16a20ac77c6ccc6a
PR0C3S <20180767@ce.pucmm.edu.do>
2022-06-22 18:35:29 -0400

    Integracion funciones de mongoDB para componentes

--- src/main/java/com/pucmm/tecnologiaEmergente/Repositorios/ComponenteRepositorio.java
+++ src/main/java/com/pucmm/tecnologiaEmergente/Repositorios/ComponenteRepositorio.java
@@ -1,14 +1,57 @@
 package com.pucmm.tecnologiaEmergente.Repositorios;
 
 import com.pucmm.tecnologiaEmergente.Models.Componente;
+import org.bson.Document;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.time.LocalDate;
+
 public interface ComponenteRepositorio extends MongoRepository<Componente, String> {
 
-    @Query("{ 'codigoComponente' : ?0, 'almacenes.codigoAlmacen' : ?1, }")
-    Componente findByCodigoComponenteAndAlmacenesCodigoAlmacen(String componente, String almacen);
+    @Aggregation(pipeline = {
+            "{$match: {\n" +
+                    "        codigoComponente: ?0\n" +
+                    "    }}",
+            "{$unwind: {\n" +
+                    "        path: \"$almacenes\"\n" +
+                    "    }}",
+            "{$group: {\n" +
+                    "        _id: {codigoComponente: \"$codigoComponente\",descripcion: \"$descripcion\"},\n" +
+                    "        balanceTotal:{$sum: \"$almacenes.balanceAlmacen\"}\n" +
+                    "    }}",
+            "{$project: {\n" +
+                    "        _id:0,\n" +
+                    "        codigoComponente: \"$_id.codigoComponente\",\n" +
+                    "        descripcion: \"$_id.descripcion\",\n" +
+                    "        balanceTotal: \"$balanceTotal\"\n" +
+                    "    }}"
+    })
+    AggregationResults<Document> findComponenteCantidad(int codigoComponente);
 
-    @Query("{ 'codigoComponente' : ?0")
-    Componente findByCodigoComponente(String componente);
+
+
+    @Aggregation(pipeline = {
+        "{$unwind: {\n" +
+                "        path: \"$ordenados\"\n" +
+                "    }}",
+            " {$match: {\n" +
+                    "        codigoComponente: 5,\n" +
+                    "        \"ordenados.fechaEntrega\":{$lte: limSupFecha}\n" +
+                    "    }}",
+            "{$group: {\n" +
+                    "        _id: {codigoComponente: \"$codigoComponente\",descripcion: \"$descripcion\"},\n" +
+                    "        totalEntregados:{$sum: \"$ordenados.cantidadComprada\"}\n" +
+                    "    }}",
+            "{$project: {\n" +
+                    "        _id:0,\n" +
+                    "        codigoComponente: \"$_id.codigoComponente\",\n" +
+                    "        descripcion: \"$_id.descripcion\",\n" +
+                    "        totalEntregados: \"$totalEntregados\"\n" +
+                    "    }}"
+    })
+    AggregationResults<Document> findComponenteOrdendos(int codigoComponente, LocalDate fecha);
+
 }
